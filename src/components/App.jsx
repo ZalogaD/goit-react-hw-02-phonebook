4467a153939031d3e14b7b39b76190ddf4c027ba
Darya Zaloha <dashulia.zaloga@gmail.com>
2023-06-01 13:04:20 +0300

    cont

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ContactForm from './ContactForm/СontactForm';
+import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import { nanoid } from 'nanoid';
